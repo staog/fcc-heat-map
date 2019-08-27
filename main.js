2edaa68b85c7b9b9c226a5c3b3e50ca7f406d99a
Milan V. Kecojević <staog@gmx.com>
2019-08-27 20:58:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+function heatmap(dataset){
+
+  let margin = 50;
+  let w = 1200;
+  let h = 480;
+  
+  const svg = d3.select("#map")
+                .append("svg")
+                .attr("align", "centre")
+                .attr("width", w)
+                .attr("height", h);
+  
+}
+
 d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json", function(json){
   const dataset = json.data;
   heatmap(dataset)
