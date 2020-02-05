595b5b1c6a876cab2bf35fed0399ca1bb89bb04a
Milan V. Kecojević <staog@gmx.com>
2020-02-05 22:45:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ function heatmap(data){
   
    console.log(data);
   
-   const variance = data.monthlyVariance;
+   const tempVariance = data.monthlyVariance;
    const base = data.baseTemperature;
-  
-   console.log(variance, base)
   
    const width = 1100;
    const height = 480;
@@ -37,8 +35,8 @@ function heatmap(data){
   
    const colors = ['#313695', '#4675B4', '#74ADD1', '#ABD9E9', '#E0F2F8', '#FEFBBE', '#FCE090', '#F5AD60', '#EF6C42', '#D6322D', '#A62527'];
   
-   const varianceMin = d3.min(variance, d => d.year);
-   const varianceMax = d3.max(variance, d => d.year);
+   const varianceMin = d3.min(tempVariance, d => d.year);
+   const varianceMax = d3.max(tempVariance, d => d.year);
   
    const main = d3.select("#main");
    
@@ -48,7 +46,7 @@ function heatmap(data){
   
    main.append("h2")
           .attr("id", "description")
-          .html(variance[0].year + " - " + variance[variance.length - 1].year + ": Base temperature " + base + "&#8451;");
+          .html(tempVariance[0].year + " - " + tempVariance[tempVariance.length - 1].year + ": Base temperature " + base + "&#8451;");
   
    const svg = main.append("svg")
                    .attr("align", "centre")
@@ -60,7 +58,7 @@ function heatmap(data){
    
    let tooltip = main.append("div")
                      .attr("id", "tooltip")
-                     .style("opacity", 1)
+                     .style("opacity", 0)
                      .text('Hello there');
   
    const xScale = d3.scaleLinear()
@@ -72,17 +70,33 @@ function heatmap(data){
                     .range(margin.top, innerHeight);
   
    let colorScale = d3.scaleQuantize()
-    .domain([d3.min(variance, (d) => d.variance), d3.max(variance, (d) => d.variance)])
+    .domain([d3.min(tempVariance, (d) => d.variance), d3.max(tempVariance, (d) => d.variance)])
     .range(colors);
   
+   const xAxis = d3.axisBottom()
+                   .scale(xScale)
+                   .tickFormat(d => d);
+  
+   const yAxis = d3.axisLeft(yScale);
+  
+   svg.append("g")
+      .attr("id", "x-axis")
+      .attr("transform", "translate(100, 500)")
+      .call(xAxis);
+
+   svg.append("g")
+      .attr("id", "y-axis")
+      .attr("transform", "translate(120, " + margin.top + ")") 
+      .call(yAxis); 
+  
    g.selectAll("rect")
-    .data(variance)
+    .data(tempVariance)
     .enter()
     .append("rect")
     .attr("class", "cell")
     .attr("x", d => xScale(d.year))
     .attr("y", d => yScale(d.month))
-    .attr("width", width / variance.length * 12)
+    .attr("width", width / tempVariance.length * 12)
     .attr("height", height / 12)
     .attr('fill', (d) => colorScale(d.variance));
   
