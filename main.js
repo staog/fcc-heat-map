9cd99fbfb4a1cd082d2d8ffbf8d30b7855e6a252
Milan V. Kecojević <staog@gmx.com>
2020-02-08 00:12:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -16,10 +16,27 @@ function heatmap(data){
    const innerWidth = width - margin.left - margin.right;
    const innerHeight = height - margin.top - margin.bottom;
   
+   const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+  
    const colors = ['#313695', '#4675B4', '#74ADD1', '#ABD9E9', '#E0F2F8', '#FEFBBE', '#FCE090', '#F5AD60', '#EF6C42', '#D6322D', '#A62527'];
   
-   const varianceMin = d3.min(tempVariance, d => d.year);
-   const varianceMax = d3.max(tempVariance, d => d.year);
+   const startYear = d3.min(tempVariance, d => d.year);
+   const endYear = d3.max(tempVariance, d => d.year);
+   const varianceMin = d3.min(tempVariance, d => d.variance);
+   const varianceMax = d3.max(tempVariance, d => d.variance);
   
    const main = d3.select("#main");
    
@@ -41,40 +58,27 @@ function heatmap(data){
    
    let tooltip = main.append("div")
                      .attr("id", "tooltip")
-                     .style("opacity", 1)
-                     .text('Hello there');
+                     .style("visibility", "hidden");
   
    const xScale = d3.scaleLinear()
-                    .domain([varianceMin, varianceMax])
+                    .domain([startYear, endYear])
                     .range([margin.right, innerWidth]);
   
    const yScale = d3.scaleBand()
-                    .domain( ['Jan',
-                              'Feb',
-                              'Mar',
-                              'Apr',
-                              'May',
-                              'Jun',
-                              'Jul',
-                              'Aug',
-                              'Sep',
-                              'Oct',
-                              'Nov',
-                              'Dec']
-                           )
+                    .domain(months)
                     .range([margin.top, innerHeight]);
   
    let colorScale = d3.scaleQuantize()
-    .domain([d3.min(tempVariance, (d) => d.variance), d3.max(tempVariance, (d) => d.variance)])
-    .range(colors);
+                      .domain([varianceMin, varianceMax])
+                      .range(colors);
   
    const xAxis = d3.axisBottom()
                    .scale(xScale)
                    .tickFormat(d => d);
   
    const yAxis = d3.axisLeft()
-                   .scale(yScale)
-                   .tickSize(0);
+                   .tickSize(0)
+                   .scale(yScale);
   
    svg.append("g")
       .attr("id", "x-axis")
@@ -111,13 +115,39 @@ function heatmap(data){
     .enter()
     .append("rect")
     .attr("class", "cell")
+    .attr('data-month', d => d.month - 1)
+    .attr('data-year', d => d.year)
+    .attr('data-temp', d => d.variance)
     .attr("x", d => xScale(d.year))
-    .attr("y", d => yScale(d.month))
+    .attr('y', (d, i) => yScale(months[d.month - 1]) - margin.top)
     .attr("width", innerWidth / tempVariance.length * 12)
     .attr("height", innerHeight / 12)
-    .attr('fill', d => colorScale(d.variance));
+    .attr('fill', d => colorScale(d.variance))
+    .on("mouseover", (d, i) => {         
+       tooltip.transition()
+              .style("visibility", "visible")
+              .attr("data-year", d.year)
+       tooltip.html(`Year: ${d.year}, Month: ${d.month}<br/>Temp: ${(base + d.variance).toFixed(1)}℃<br/>Variance: ${d.variance.toFixed(2)}`)
+              .style('left', `${d3.event.pageX - 460}px`)
+              .style('top', `${d3.event.pageY - 40}px`)
+     })
+     .on("mouseout", d => {
+       tooltip.transition()
+              .style("visibility", "hidden");
+     });
   
-  };
+   const legend = main.append("div")
+                      .selectAll("rect")
+                      .attr("id", "legend")
+                      .data(colors)
+                      .enter()
+                      .append("rect")
+                      .attr("x", 100)
+                      .attr("y", 200)
+                      .attr("width", 280)
+                      .attr("height", 28)
+                      .style("fill", d => d);
+};
  
  d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json", function(json){
    const data = json;
